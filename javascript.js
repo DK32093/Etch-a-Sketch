2c416e0e38e2de5cf7230fcd95c24b8a9c27d1af
DK32093 <107143112+DK32093@users.noreply.github.com>
2024-08-09 13:35:45 -0400

    add event listener and button

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 
 const grid = document.querySelector(".grid");
 
-// create grid
+// create initial grid
 for (i = 1; i <= 16; i++) {
     let column = document.createElement("div");
     grid.appendChild(column);
@@ -12,3 +12,15 @@ for (i = 1; i <= 16; i++) {
         newDiv.setAttribute("class", "box");
     }
 }
+
+// mouseenter event listener
+let boxes = document.querySelectorAll(".box"); //retrieve all of class "box"
+
+let changeBackground = function(e) { // create function to change the background
+    e.target.style.background = "blue";
+};
+
+boxes.forEach(function(box) { // apply listener with function to each element
+    box.addEventListener("mouseenter", changeBackground)
+});
+
